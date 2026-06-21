38404fb831508ee88c4ccd33839051b970acb670
picafern-commits <pica.fern@gmail.com>
2026-06-21 12:09:22 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-// Compatibilidade: a app agora usa /firebase-messaging-sw.js como Service Worker principal.
-// Este ficheiro fica simples para não misturar cache PWA com Firebase Messaging.
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
